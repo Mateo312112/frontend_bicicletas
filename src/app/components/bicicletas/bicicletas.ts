fe4dde03ea8927008cc1a4ac053c64f8618653fb
Mateo312112 <suarezmateo3121@gmail.com>
2026-04-02 14:30:07 -0500

    estructura inicical frontend

---
+++ src/app/components/bicicletas/bicicletas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bicicletas',
+  imports: [],
+  templateUrl: './bicicletas.html',
+  styleUrl: './bicicletas.css',
+})
+export class Bicicletas {}
---
+++ src/app/components/detalle-venta/detalle-venta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-venta',
+  imports: [],
+  templateUrl: './detalle-venta.html',
+  styleUrl: './detalle-venta.css',
+})
+export class DetalleVenta {}
