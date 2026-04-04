8544edd26c7f785489a7ee34e8dc3ddf5ce07c83
Mateo312112 <suarezmateo3121@gmail.com>
2026-04-04 16:41:54 -0500

    se agregaron reportes

---
+++ src/app/services/reporte.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ReporteVentaDia, ReporteTopProducto, ReporteStock } from '../models/venta.model';
+
+const API = 'http://localhost:8080/api/reportes';
+
+@Injectable({ providedIn: 'root' })
+export class ReporteService {
+  constructor(private http: HttpClient) {}
+
+  getVentasPorDia(): Observable<ReporteVentaDia[]> { return this.http.get<ReporteVentaDia[]>(`${API}/ventas-por-dia`); }
+  getVentasPorDiaRango(inicio: string, fin: string): Observable<ReporteVentaDia[]> {
+    return this.http.get<ReporteVentaDia[]>(`${API}/ventas-por-dia/rango?inicio=${inicio}&fin=${fin}`);
+  }
+  getTopProductos(): Observable<ReporteTopProducto[]> { return this.http.get<ReporteTopProducto[]>(`${API}/top-productos`); }
+  getStock(): Observable<ReporteStock[]> { return this.http.get<ReporteStock[]>(`${API}/stock`); }
+  getStockCritico(): Observable<ReporteStock[]> { return this.http.get<ReporteStock[]>(`${API}/stock/critico`); }
+}
