247e2378a6818d99aceba422c8ee0a8cdc38055c
Mateo312112 <suarezmateo3121@gmail.com>
2026-04-04 16:37:11 -0500

    funcionalidad inventario hecha

---
+++ src/app/services/inventario.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Inventario } from '../models/inventario.model';
+
+const API = 'http://localhost:8080/api/inventario';
+
+@Injectable({ providedIn: 'root' })
+export class InventarioService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Inventario[]> { return this.http.get<Inventario[]>(API); }
+  getByBicicleta(id: number): Observable<Inventario> { return this.http.get<Inventario>(`${API}/bicicleta/${id}`); }
+  getAlertas(): Observable<Inventario[]> { return this.http.get<Inventario[]>(`${API}/alerta`); }
+  create(idBicicleta: number, cantidadInicial: number, stockMinimo: number): Observable<Inventario> {
+    return this.http.post<Inventario>(`${API}?idBicicleta=${idBicicleta}&cantidadInicial=${cantidadInicial}&stockMinimo=${stockMinimo}`, {});
+  }
+  updateCantidad(idBicicleta: number, nuevaCantidad: number): Observable<Inventario> {
+    return this.http.put<Inventario>(`${API}/bicicleta/${idBicicleta}?nuevaCantidad=${nuevaCantidad}`, {});
+  }
+}
