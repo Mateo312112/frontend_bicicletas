0ff0d81f43e5679a9743f9a89f90c789504bf59c
Johan Silva <146844320+Kiorenc3@users.noreply.github.com>
2026-04-05 23:07:01 -0500

    Revert "feat: agregar rutas y navegacion frontend"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,3 @@
 import { Routes } from '@angular/router';
-import { ClientesComponent } from './components/clientes/clientes';
-import { BicicletasComponent } from './components/bicicletas/bicicletas';
-import { DetalleVentaComponent } from './components/detalle-venta/detalle-venta';
-import { Inventario } from './components/inventario/inventario';
-import { Ventas } from './components/ventas/ventas';
-import { Reportes } from './components/reportes/reportes';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
-  { path: 'clientes', component: ClientesComponent },
-  { path: 'bicicletas', component: BicicletasComponent },
-  { path: 'detalle-venta', component: DetalleVentaComponent },
-  { path: 'inventario', component: Inventario },
-  { path: 'ventas', component: Ventas },
-  { path: 'reportes', component: Reportes }
-];
+export const routes: Routes = [];
